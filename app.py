4b56879ee6473c55ba880968e37d3f0db4e4e85b
Zakhar Kogan <36503576+zakhar-kogan@users.noreply.github.com>
2024-02-22 09:42:49 +0200

    Updated with file conversion

--- app.py
+++ app.py
@@ -3,7 +3,28 @@ import numpy as np
 import torch
 from transformers import pipeline
 
+from pydub import AudioSegment
+
 class InferlessPythonModel:
+
+    def convert_to_mp3(self, file_path):
+        # Check the file extension
+        file_extension = file_path.split('.')[-1]
+
+        if file_extension.lower() not in ['mp3', 'wav', 'flac']:
+            # Load the audio file
+            audio = AudioSegment.from_file(file_path, format=file_extension)
+
+            # Define the new file path
+            new_file_path = file_path.rsplit('.', 1)[0] + '.mp3'
+
+            # Export the audio file in MP3 format
+            audio.export(new_file_path, format='mp3')
+
+            return new_file_path
+
+        # If the file is already in MP3 format, return the original file path
+        return file_path
 
     def initialize(self):
         self.generator = pipeline(
@@ -14,7 +35,7 @@ class InferlessPythonModel:
         )
 
     def infer(self, inputs):
-        audio_url = inputs["audio_url"]
+        audio_url = self.convert_to_mp3(inputs["audio_url"])
 
         pipeline_output = self.generator(audio_url)
         return {"transcribed_output": pipeline_output["text"] }
